174d507b6db630d9d4db87a4529b98d34642780b
LonG <bunlongly03@gmail.com>
2023-10-23 09:43:35 +0700

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+// import HomeScreen from './screens/HomeScreen'
+
 
 const App = () => {
   return (
-    <h1>Welcome to ProShop</h1>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet/>
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './store'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={ <App/> }>
+      <Route index={true} path="/" element={<HomeScreen/>}/>
+      <Route  path="/product/:id" element={<ProductScreen/>}/>
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
