77e778f79d13fb22258e749a8bad16924642cd9f
LonG <bunlongly03@gmail.com>
2023-10-28 15:58:49 +0700

    User Logout, UserRegister

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import React from "react";
 import { Badge, Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
 // import { FaShoppingCart, FaUser } from "react-icons/fa";
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
   const { userInfo } = useSelector((state) => state.auth);
 
-  const logoutHandler = () => {
-    console.log("log out");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <header>
