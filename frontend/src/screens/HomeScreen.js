ac16cc702798599ebdeb8194b44dea372c2fe532
LonG <bunlongly03@gmail.com>
2023-11-13 16:15:27 +0700

    Paginate Products

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,36 +1,37 @@
-
-import React from 'react'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
-import Loader from '../components/Loader'
-import Message from '../components/Message'
-import { useGetProductsQuery } from '../slices/productsApiSlice'
-
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 const HomeScreen = () => {
-  const {data:products, isLoading, error} = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
   return (
     <>
-    {isLoading ? (
-      <Loader/>
-    ) : error ? (
-    <Message variant={'danger'}>
-      (error?.data?.message || error.error)
-    </Message>
-    ) :
-     (<>
-         <h1>Latest Products</h1>
-         <Row>
-           {products.map((product) => (
-             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-               <Product product={product} />
-             </Col>
-           ))}
-         </Row>
-    </>) }
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant={"danger"}>
+          (error?.data?.message || error.error)
+        </Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {data.products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
